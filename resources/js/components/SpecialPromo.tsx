c7b1b9bc417b1b5067c2a64654e30b69e15c2779
soga <durojaiyesoga@gmail.com>
2025-06-04 21:29:20 +0100

    EDITING UI

--- resources/js/components/SpecialPromo.tsx
+++ resources/js/components/SpecialPromo.tsx
@@ -11,6 +11,97 @@ export default function SpecialPromo() {
                         <h3 className='text-3xl text-center py-2  text-white bg-green-600 font-bold rounded-lg mr-70'>20% OFF</h3>
                 </div>
             </div>
+            <div className="flex flex-col-2 gap-6 bg-green-100 rounded-3xl h-100">
+                <div className="px-35 py-25">
+                    <h3 className='text-2xl text-black font-bold mb-4'>DRUG NAME</h3>
+                    <p className='text-lg font-medium mb-8 text-gray-700'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. <br /> Ad totam nemo alias sequi, quisquam, asperiores iure <br /> quos quae voluptatibus voluptas</p>
+                        <h3 className='text-3xl text-center py-2  text-white bg-green-600 font-bold rounded-lg mr-70'>20% OFF</h3>
+                </div>
+            </div>
+            <div className="flex flex-col-2 gap-6 bg-green-100 rounded-3xl h-100">
+                <div className="px-35 py-25">
+                    <h3 className='text-2xl text-black font-bold mb-4'>DRUG NAME</h3>
+                    <p className='text-lg font-medium mb-8 text-gray-700'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. <br /> Ad totam nemo alias sequi, quisquam, asperiores iure <br /> quos quae voluptatibus voluptas</p>
+                        <h3 className='text-3xl text-center py-2  text-white bg-green-600 font-bold rounded-lg mr-70'>20% OFF</h3>
+                </div>
+            </div>
+            <div className="flex flex-col-2 gap-6 bg-green-100 rounded-3xl h-100">
+                <div className="px-35 py-25">
+                    <h3 className='text-2xl text-black font-bold mb-4'>DRUG NAME</h3>
+                    <p className='text-lg font-medium mb-8 text-gray-700'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. <br /> Ad totam nemo alias sequi, quisquam, asperiores iure <br /> quos quae voluptatibus voluptas</p>
+                        <h3 className='text-3xl text-center py-2  text-white bg-green-600 font-bold rounded-lg mr-70'>20% OFF</h3>
+                </div>
+            </div>
+            <div className="flex flex-col-2 gap-6 bg-green-100 rounded-3xl h-100">
+                <div className="px-35 py-25">
+                    <h3 className='text-2xl text-black font-bold mb-4'>DRUG NAME</h3>
+                    <p className='text-lg font-medium mb-8 text-gray-700'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. <br /> Ad totam nemo alias sequi, quisquam, asperiores iure <br /> quos quae voluptatibus voluptas</p>
+                        <h3 className='text-3xl text-center py-2  text-white bg-green-600 font-bold rounded-lg mr-70'>20% OFF</h3>
+                </div>
+            </div>
+            <div className="flex flex-col-2 gap-6 bg-green-100 rounded-3xl h-100">
+                <div className="px-35 py-25">
+                    <h3 className='text-2xl text-black font-bold mb-4'>DRUG NAME</h3>
+                    <p className='text-lg font-medium mb-8 text-gray-700'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. <br /> Ad totam nemo alias sequi, quisquam, asperiores iure <br /> quos quae voluptatibus voluptas</p>
+                        <h3 className='text-3xl text-center py-2  text-white bg-green-600 font-bold rounded-lg mr-70'>20% OFF</h3>
+                </div>
+            </div>
+            <div className="flex flex-col-2 gap-6 bg-green-100 rounded-3xl h-100">
+                <div className="px-35 py-25">
+                    <h3 className='text-2xl text-black font-bold mb-4'>DRUG NAME</h3>
+                    <p className='text-lg font-medium mb-8 text-gray-700'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. <br /> Ad totam nemo alias sequi, quisquam, asperiores iure <br /> quos quae voluptatibus voluptas</p>
+                        <h3 className='text-3xl text-center py-2  text-white bg-green-600 font-bold rounded-lg mr-70'>20% OFF</h3>
+                </div>
+            </div>
+            <div className="flex flex-col-2 gap-6 bg-green-100 rounded-3xl h-100">
+                <div className="px-35 py-25">
+                    <h3 className='text-2xl text-black font-bold mb-4'>DRUG NAME</h3>
+                    <p className='text-lg font-medium mb-8 text-gray-700'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. <br /> Ad totam nemo alias sequi, quisquam, asperiores iure <br /> quos quae voluptatibus voluptas</p>
+                        <h3 className='text-3xl text-center py-2  text-white bg-green-600 font-bold rounded-lg mr-70'>20% OFF</h3>
+                </div>
+            </div>
+            <div className="flex flex-col-2 gap-6 bg-green-100 rounded-3xl h-100">
+                <div className="px-35 py-25">
+                    <h3 className='text-2xl text-black font-bold mb-4'>DRUG NAME</h3>
+                    <p className='text-lg font-medium mb-8 text-gray-700'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. <br /> Ad totam nemo alias sequi, quisquam, asperiores iure <br /> quos quae voluptatibus voluptas</p>
+                        <h3 className='text-3xl text-center py-2  text-white bg-green-600 font-bold rounded-lg mr-70'>20% OFF</h3>
+                </div>
+            </div>
+            <div className="flex flex-col-2 gap-6 bg-green-100 rounded-3xl h-100">
+                <div className="px-35 py-25">
+                    <h3 className='text-2xl text-black font-bold mb-4'>DRUG NAME</h3>
+                    <p className='text-lg font-medium mb-8 text-gray-700'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. <br /> Ad totam nemo alias sequi, quisquam, asperiores iure <br /> quos quae voluptatibus voluptas</p>
+                        <h3 className='text-3xl text-center py-2  text-white bg-green-600 font-bold rounded-lg mr-70'>20% OFF</h3>
+                </div>
+            </div>
+            <div className="flex flex-col-2 gap-6 bg-green-100 rounded-3xl h-100">
+                <div className="px-35 py-25">
+                    <h3 className='text-2xl text-black font-bold mb-4'>DRUG NAME</h3>
+                    <p className='text-lg font-medium mb-8 text-gray-700'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. <br /> Ad totam nemo alias sequi, quisquam, asperiores iure <br /> quos quae voluptatibus voluptas</p>
+                        <h3 className='text-3xl text-center py-2  text-white bg-green-600 font-bold rounded-lg mr-70'>20% OFF</h3>
+                </div>
+            </div>
+            <div className="flex flex-col-2 gap-6 bg-green-100 rounded-3xl h-100">
+                <div className="px-35 py-25">
+                    <h3 className='text-2xl text-black font-bold mb-4'>DRUG NAME</h3>
+                    <p className='text-lg font-medium mb-8 text-gray-700'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. <br /> Ad totam nemo alias sequi, quisquam, asperiores iure <br /> quos quae voluptatibus voluptas</p>
+                        <h3 className='text-3xl text-center py-2  text-white bg-green-600 font-bold rounded-lg mr-70'>20% OFF</h3>
+                </div>
+            </div>
+            <div className="flex flex-col-2 gap-6 bg-green-100 rounded-3xl h-100">
+                <div className="px-35 py-25">
+                    <h3 className='text-2xl text-black font-bold mb-4'>DRUG NAME</h3>
+                    <p className='text-lg font-medium mb-8 text-gray-700'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. <br /> Ad totam nemo alias sequi, quisquam, asperiores iure <br /> quos quae voluptatibus voluptas</p>
+                        <h3 className='text-3xl text-center py-2  text-white bg-green-600 font-bold rounded-lg mr-70'>20% OFF</h3>
+                </div>
+            </div>
+            <div className="flex flex-col-2 gap-6 bg-green-100 rounded-3xl h-100">
+                <div className="px-35 py-25">
+                    <h3 className='text-2xl text-black font-bold mb-4'>DRUG NAME</h3>
+                    <p className='text-lg font-medium mb-8 text-gray-700'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. <br /> Ad totam nemo alias sequi, quisquam, asperiores iure <br /> quos quae voluptatibus voluptas</p>
+                        <h3 className='text-3xl text-center py-2  text-white bg-green-600 font-bold rounded-lg mr-70'>20% OFF</h3>
+                </div>
+            </div>
         </section>
 
 
